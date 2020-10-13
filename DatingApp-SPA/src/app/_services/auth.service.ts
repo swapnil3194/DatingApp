95842f4a006655813c772c3586d9b5f6e465eebb
DESKTOP-2B45DKG\Swapnil <swapnilkhopkar1994@gmail.com>
2020-10-13 15:22:12 +0530

    End Of Section 4

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class AuthService {
+baseUrl = 'http://localhost:5000/api/auth/';
+
+constructor(private http: HttpClient) { }
+
+login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+       map((repsonse: any) => {
+           const user = repsonse;
+           if (user) {
+               localStorage.setItem('token', user.token);
+           }
+       })
+    );
+}
+
+register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+}
+
+}
