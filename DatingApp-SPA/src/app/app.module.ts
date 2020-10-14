199dc686138aa081fa7a8b9fc6c7549731f76ea0
DESKTOP-2B45DKG\Swapnil <swapnilkhopkar1994@gmail.com>
2020-10-14 18:27:12 +0530

    End Of Season 5

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
     NavComponent,
       HomeComponent,
@@ -23,7 +24,8 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [AuthService],
+  providers: [AuthService,
+  ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
